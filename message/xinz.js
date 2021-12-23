3a111c61c4b365ecf38a05d85e7b4cec0b01b2d4
Rtxc7 <92695382+Rtxc7@users.noreply.github.com>
2021-12-23 10:46:28 +0700

    Add files via upload

--- message/xinz.js
+++ message/xinz.js
@@ -1519,19 +1519,20 @@ _Silahkan tunggu file media sedang dikirim mungkin butuh beberapa menit_`
                 }
             }
                 break
-                case prefix+"asupan": // xteam
+                case prefix+"asupan": {// xteam
                     if (!isPremium) return reply(mess.OnlyPrem)
-                    get_result = await fetchJson(`https://api.xteam.xyz/asupan/ptl?APIKEY=91e9b333684574e2`)
-                    get_result = get_result.result
-                    get_audio = await getBuffer(get_result.link)
-                    await xinz.sendMessage(from, get_audio, video, { mimetype: 'video/mp4', filename: `${get_result.username}.mp4`, quoted: fvn, caption: "NIH ASUPAN" })
+                   let get_result = await fetchJson(`https://api.xteam.xyz/asupan/ptl?APIKEY=91e9b333684574e2`)
+                   let get_resultt = get_result.result
+                   let get_audio = await getBuffer(get_resultt.link)
+                    await xinz.sendMessage(from, get_audio, video, { mimetype: 'video/mp4', filename: `${get_resultt.username}.mp4`, quoted: msg, caption: "NIH ASUPAN" })
+                    }
                     break
                     case prefix+"storyanime": // xteam
                     if (!isPremium) return reply(mess.OnlyPrem)
-                    get_result = await fetchJson(`https://api.xteam.xyz/asupan/wibu?APIKEY=91e9b333684574e2`)
-                    get_result = get_result.result
-                    get_audio = await getBuffer(get_result.link)
-                    await xinz.sendMessage(from, get_audio, video, { mimetype: 'video/mp4', filename: `${get_result.username}.mp4`, quoted: fvn, caption: "NIH STORY ANIMENYA" })
+                    let get_result = await fetchJson(`https://api.xteam.xyz/asupan/wibu?APIKEY=91e9b333684574e2`)
+                    let get_resultt = get_result.result
+                    let get_audio = await getBuffer(get_resultt.link)
+                    await xinz.sendMessage(from, get_audio, video, { mimetype: 'video/mp4', filename: `${get_resultt.username}.mp4`, quoted: fvn, caption: "NIH STORY ANIMENYA" })
                     break;
             case prefix+'pinterest': case prefix+'pinsearch': case prefix+'pinterestsearch':{
                 if (!isPremium) return reply(mess.OnlyPrem)
